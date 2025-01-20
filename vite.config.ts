e6e751604d372c12451e1fc1787e140b9bbc748c
DmytroVoitovych <begunec23@gmail.com>
2025-01-20 12:47:26 +0200

    added base configuration in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Frontend-Quiz-app/' : '/',
   plugins: [react()],
 });
